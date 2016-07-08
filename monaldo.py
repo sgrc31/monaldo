52230aa486db7b8646ef6fdf9f182a4b887e80e9
Simone Guercio <s.guercio@gmail.com>
2016-07-08 12:35:19 +0200

    Initial commit

---
+++ monaldo.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import os
+from flask import Flask, render_template
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+
+basedir_booksdb = os.path.abspath(os.path.dirname('ebooks/metadata.db'))
+basedir_usersdb = os.path.abspath(os.path.dirname('.'))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_BINDS'] = {'booksdb': 'sqlite:///{}'.format(os.path.join(basedir_booksdb, 'metadata.db')),
+                                  'usersdb': 'sqlite:///{}'.format(os.path.join(basedir_usersdb, 'users.sqlite'))}
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+
+db = SQLAlchemy(app)
+manager = Manager(app)
+
+
+#######################
+# Classi database
+#######################
+class Authors(db.Model):
+    __bind_key__ = 'booksdb'
+    __tablename__ = 'authors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+
+    #def __repr__(self):
+    #    self.name = self.name.replace(' ', '_')
+    #    return '<Autore {}>'.format(self.name)
+
+
+class Books(db.Model):
+    __bind_key__ = 'booksdb'
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    author_sort = db.Column(db.Text)
+
+    #def __repr__(self):
+    #    return 'stocazzo_{}'.format(self.title)
+
+    
+############
+# Routes
+############
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#@app.route('/authors')
+#def authors():
+#    autori = Authors.query.all()
+#    nomi_autori_slug = []
+#    for autore in autori:
+#        autore.name = autore.name.replace(' ', '_').lower()
+#        nomi_autori_slug.append(autore.name)
+#    return render_template('authors.html', names = nomi_autori_slug)
+
+@app.route('/authors')
+def authors():
+    return render_template('authors.html', names = Authors.query.all())
+
+@app.route('/authors/<author_name>')
+def author_page(author_name):
+    return render_template('author_page.html', author_name = author_name)
+
+@app.route('/user/<nome>')
+def user(nome):
+    return render_template('user.html', name=nome)
+
+if __name__ == '__main__':
+    manager.run()
